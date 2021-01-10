ab110c07a6cfb44dd6106cedc4108f3cf5b6e73d
Krocketeer <Krocketeer@github.com>
2021-01-10 02:20:27 -0800

    deployed to Firebase

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import About from "./About"
 import Home from "./Home"
 import ProjectTemplate from './projects/ProjectTemplate'
@@ -9,6 +9,6 @@ export default function Routing() {
         <Route path="/" exact component={Home}/>
         <Route path="/about" exact component={About} />
         <Route path="/blink" exact component={ProjectTemplate} />
-        {/*<Route path="*" component={About}/>*/}
+        <Route path="*" component={Home}/>
     </Switch>
 }
