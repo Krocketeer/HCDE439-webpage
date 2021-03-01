42224dedd3e6102ad722a480aed1992c6d3ff2c3
Krocketeer <Krocketeer@github.com>
2021-03-01 02:11:18 -0800

    added Assignment 6 code

--- ArduinoCode/WebCommunication/CommentedCode.cpp
+++ ArduinoCode/WebCommunication/CommentedCode.cpp
@@ -2,85 +2,105 @@
  * Kenny "Ackerson" Le
  * 2/25/21
  * Assignment 6: Talking to the Web!
- * Description: Allows the control of a LED with a joystick
- * and a webpage using the P5.js library
+ * Description: Handles Arduino side of communication with P5
+ * Allows the control of a LED with a joystick and a webpage using the P5.js library
  */
 
+// Import statement to make Arduino work with Clion
 #include <Arduino.h>
-const int VRx = A0;
-const int VRy = A1;
+const int VRx = A0;      // Pin of X-axis control for joystick
+const int VRy = A1;      // Pin of X-axis control for joystick
 
-const int redLED = 11;
-const int greenLED = 9;
-const int blueLED = 6;
+const int redLED = 11;   // Pin of red diode of RGB LED
+const int greenLED = 9;  // Pin of green diode of RGB LED
+const int blueLED = 6;   // Pin of blue diode of RGB LED
 
+// Function declarations
 void sendSerial();
 void readSerial();
 void controlLED(int byte);
 
 void setup() {
+    // initialize each LED pin as an output
     pinMode(redLED, OUTPUT);
     pinMode(greenLED, OUTPUT);
     pinMode(blueLED, OUTPUT);
 
+    // initialize each joystick pin as an input
     pinMode(VRx, INPUT);
     pinMode(VRy, INPUT);
 
+    // initialize serial monitor with baud 9600
     Serial.begin(9600);
+    // waits .01 seconds max for serial data before timeout
     Serial.setTimeout(10);
 }
 
 void loop() {
+    // calls the following functions to loop over
     sendSerial();
     readSerial();
 }
 
+// sends information from the joystick over serial to P5
 void sendSerial() {
+    // X and Y Coordinates of the joystick
     int xCoord = analogRead(VRx);
-//    int yCoord = analogRead(VRy);
-//    yCoord = map(yCoord, 0, 1023, 1023, 0);
+    // inverses the direction of up and down for the joystick
     int yCoord = map(analogRead(VRy), 0, 1023, 1023, 0);
 
+    // Prints the coordinates to serial monitor in the format of [xCoord, yCoord]
     Serial.print("[");
     Serial.print(xCoord);
     Serial.print(",");
     Serial.print(yCoord);
     Serial.println("]");
+
+    // delays 0.1 seconds
     delay(100);
 }
 
+// receives information from P5 to serial
 void readSerial() {
+    // If there is serial information available
     if (Serial.available() > 0) {
+        // read the serial information
         int inByte = Serial.read();
+        // Print serial information
         Serial.print("inByte value: ");
         Serial.println(inByte);
+
+        // calls function to control LED based on serial information read in
         controlLED(inByte);
     }
 }
 
+// function to control LEDs
 void controlLED(int byte) {
+    // Switch case to determine what to do with byte
     switch (byte) {
-        case 1: // purple LED on
+        case 1: // Turns RGB LED purple
             analogWrite(redLED, 127);
             analogWrite(greenLED, 0);
             analogWrite(blueLED, 127);
             break;
-        case 2: // red LED on
+        case 2: // Turns RGB LED red
             analogWrite(redLED, 255);
             analogWrite(greenLED, 0);
             analogWrite(blueLED, 0);
             break;
-        case 3: // green LED on
+        case 3: // Turns RGB LED green
             analogWrite(greenLED, 255);
             analogWrite(redLED, 0);
             analogWrite(blueLED, 0);
             break;
-        case 4: // blue LED on
+        case 4: // turns RGB led blue
             analogWrite(blueLED, 255);
             analogWrite(greenLED, 0);
             analogWrite(redLED, 0);
             break;
         default:
+            // If any other byte is read in, print byte to Serial Monitor
             Serial.print("Error, unrecognized byte: ");
             Serial.println(byte);
             break;
--- p5/p5-WebCommmunication/CommentedCode.js
+++ p5/p5-WebCommmunication/CommentedCode.js
@@ -1,14 +1,6 @@
-/*
- * Kenny "Ackerson" Le
- * 2/25/21
- * Assignment 6: Talking to the Web!
- * Description: Handles P5 side of communication with Arduino
- * Program allows the control of a LED with a joystick and a webpage using the P5.js library
- */
-
 let serial; // variable to hold an instance of the serialport library
-const portName = '/dev/tty.usbserial-01677F32' //rename to the name of your port
-let dataArray = []; //some data coming in over serial!
+let dataArray = []; // some data coming in over serial!
+const portName = '/dev/tty.usbserial-01677F32' // port name
 
 function setup() {
     serial = new p5.SerialPort();              // make a new instance of the serialport library
@@ -22,7 +14,7 @@ function setup() {
     serial.list();                             // list the serial ports
     serial.open(portName);                     // open a serial port
     createCanvas(windowWidth, windowHeight);   // creates a canvas that's the size of the browser's width and height
-    background(0x08, 0x16, 0x40);
+    background(0x08, 0x16, 0x40);              // set the background to navy blue
 }
 
 // get the list of ports:
@@ -34,22 +26,27 @@ function printList(portList) {
     }
 }
 
+// prints server is connected
 function serverConnected() {
     print('connected to server.');
 }
 
+// prints serial port is opened
 function portOpen() {
     print('the serial port opened.')
 }
 
+// prints there's an error with serial port
 function serialError(err) {
     print('Something went wrong with the serial port. ' + err);
 }
 
+// prints serial port is closed
 function portClose() {
     print('The serial port closed.');
 }
 
+// Receives serial information from Arduino and converts to JSON
 function serialEvent() {
     if (serial.available()) {
         let dataString = serial.readLine(); // read in some serial
@@ -67,50 +64,72 @@ function serialEvent() {
     }
 }
 
+// Displays information on the page
 function draw() {
     stroke('rgba(0,80,255,0.5)'); // blue
     graphPosition(dataArray[0], dataArray[1]);
 }
 
+// Graphs the X and Y coordinates of the joystick's position
 function graphPosition(xCoord, yCoord) {
-    // make a queue of positions and map that
+    // Limits the X and Y Coords to 0 and 1023
     let yPos = map(yCoord,0, 1023,0, height);
     let xPos = map(xCoord, 0, 1023,0, width);
+
+    // Set background to Navy Blue
     background(0x08, 0x16, 0x40);
+
+    // Draw 4 ellipses, one in each corner
     plotLightPositions();
+
+    // Creates a white circle that corresponds to the joystick's position
     fill(255, 255, 255);
     ellipse(xPos, yPos, 100, 100);
+
+    // Determines what LED to turn on based on the joystick's position
     turnOnLED(xPos, yPos);
 }
 
+// Draw 4 ellipses, one in each corner for Purple, Red, Green, and Blue
 function plotLightPositions() {
+    // Top Left - Purple
     fill(255, 0, 255);
     ellipse(150, 150, 150, 150);
+
+    // Top Right - Red
     fill(255, 0, 0);
     ellipse(width - 150, 150, 150, 150);
+
+    // Bottom Left — Green
     fill(0, 255, 0);
     ellipse(150, height - 150, 150, 150);
+
+    // Bottom Right — Blue
     fill(0, 0, 255);
     ellipse(width - 150, height - 150, 150, 150);
 }
 
+// Determines what LED to turn on based on the joystick's position
 function turnOnLED(xPos, yPos) {
+    // If joystick ellipse is in top left corner, send int 1 over serial to Arduino
     if (xPos <= 250 && xPos >= 50 && yPos <= 250 && yPos >= 50) {
         serial.write(1);
         console.log("You're in area 1")
     }
 
-    // width - 150 + 100 = width - 50
+    // If joystick ellipse is in top right corner, send int 2 over serial to Arduino
     if (xPos <= width - 50 && xPos >= width - 250 && yPos <= 250 && yPos >= 50) {
         serial.write(2);
         console.log("You're in area 2")
     }
 
+    // If joystick ellipse is in bottom left corner, send int 3 over serial to Arduino
     if (xPos <= 250 && xPos >= 50 && yPos <= height - 50 && yPos >= height - 250) {
         serial.write(3);
         console.log("You're in area 3")
     }
 
+    // If joystick ellipse is in bottom right corner, send int 4 over serial to Arduino
     if (xPos <= width - 50 && xPos >= width - 250 && yPos <= height - 50 && yPos >= height - 250) {
         serial.write(4);
         console.log("You're in area 4")
